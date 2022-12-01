1d5edb5683ef16910e54c06a9d6652168e86c783
Jaymie O'Brien <jaymie.obrien@preciamolen.ie>
2022-12-01 15:16:17 +0000

    testing automation based on git pushes

--- Utils/Build_10.4.bat
+++ Utils/Build_10.4.bat
@@ -1,5 +1,6 @@
 rem Verify and change to the path for your rsvars.bat as needed
 rem The default for 10.4 Sydney is C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\rsvars.bat
+rem Maybe try work on a D11 project next?
 call "C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\rsvars.bat"
 
 rem The first expected parameter is the target work folder
