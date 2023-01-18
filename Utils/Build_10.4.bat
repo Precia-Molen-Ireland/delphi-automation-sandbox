9b5a58a6d696c45185afd56642c4d7cdb9f8d9cf
Jaymie O'Brien <jaymie.obrien@preciamolen.ie>
2023-01-18 15:59:09 +0000

    change path to rsvars

--- Utils/Build_10.4.bat
+++ Utils/Build_10.4.bat
@@ -1,7 +1,7 @@
 rem Verify and change to the path for your rsvars.bat as needed
 rem The default for 10.4 Sydney is C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\rsvars.bat
 rem Maybe try work on a D11 project next?
-call "C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\rsvars.bat"
+call "C:\Delphi 11\bin\rsvars.bat"
 
 rem The first expected parameter is the target work folder
 cd /D %1
